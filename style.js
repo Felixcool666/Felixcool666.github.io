00726639539bfbd9d1d60d03a9394c439f586964
felix <tengfei501@163.com>
2023-04-06 22:39:08 -0300

    no message

--- style.js
+++ style.js
@@ -1,33 +1,40 @@
+// /*Notifications and Conditional*/
+let submitButton = document.getElementById("submit");
+if (submitButton) { // if the page contains the submit button (Prospects page)
+    // add an event listener to the submit button
+    submitButton.addEventListener("click", submitMessage);
+}
 
-                /*change color*/
-                document.getElementById("color").addEventListener("click", change_color);
-                document.getElementById("color").addEventListener("click1", change_color);
-                document.getElementById("color").addEventListener("click2", change_color);
-                document.getElementById("color").addEventListener("click3", change_color);
-                /*Notifications and Conditional*/
-                document.getElementById("submit").addEventListener("click", submitMessage);
-                function submitMessage() {
-                    let FirstName = document.getElementById("FirstName").value;
-                    let LastName = document.getElementById("LastName").value;
-                    let age = document.getElementById("age").value;
+// function to be called when the submit button is clicked
+function submitMessage() {
+    // read the values of the input fields
+    let FirstName = document.getElementById("FirstName").value;
+    let LastName = document.getElementById("LastName").value;
+    let age = document.getElementById("age").value;
 
-                    if(age < 19){
-                        alert("Your are not old enough to register for submitting the message");
-                    } else {
-                        alert("Thank you for registering" + FirstName +" " +LastName + "for the course");
-                    }
-                }
-                /*change color*/
-                function change_color() {
-                    document.body.style.backgroundColor = "red";
-                }
-                function change_color() {
-                    document.body.style.backgroundColor = "blue";
-                }
-                function change_color() {
-                    document.body.style.backgroundColor = "pink";
-                }
-                function change_color() {
-                    document.body.style.backgroundColor = "lightgray";
-                }
-            
+    // check if the user is old enough to register
+    if (age < 18) {
+        alert("You are too young, these are just difficult for you, wait a few years to learn.");
+    } else {
+        alert("Thank you for your understanding and interest in learning about the program" + FirstName + " " + LastName );
+    }
+}
+// /*change color prospects.index */
+let button = document.getElementById('change-color-btn');
+
+// make sure the button exists
+if (button) {
+    button.addEventListener('click', change_color);
+}
+
+// function to change the background color
+function change_color() {
+    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff'];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+
+    document.body.style.backgroundColor = randomColor;
+};
+    
+
+                
